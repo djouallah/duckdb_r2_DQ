4c1f9d30ffdf1a298238f8948b2b7360d58ca0c1
Mimoune <djouallah@users.noreply.github.com>
2022-10-07 15:47:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ fs1 = s3fs.S3FileSystem(
          'endpoint_url': st.secrets["endpoint_url_secret"] 
       }
    )
-lineitem = ds.dataset(base, filesystem=fs1,format="parquet",partitioning="hive")
-st.write(lineitem.schema)
-SQL = st.text_input('Write a SQL Query', 'select  *  from lineitem where year = 2000 limit 1')
 con=duckdb.connect()
+xxx = ds.dataset(base, filesystem=fs1,format="parquet",partitioning="hive")
+st.write(lineitem.schema)
+con.register('lineitem',xxx)
+
+SQL = st.text_input('Write a SQL Query', 'select  *  from lineitem where year = 2000 limit 1')
+
 df = con.execute(SQL).df()
 st.write(df)

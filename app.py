c70e0693805c9263f1e091c4bc1ff2b8468447d9
Mimoune <djouallah@users.noreply.github.com>
2022-10-07 15:06:38 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import duckdb
 import s3fs
 import pyarrow.dataset as ds
-
 base = "delta/lineitem/"
 fs1 = s3fs.S3FileSystem(
       key=  st.secrets["aws_access_key_id_secret"],
@@ -12,9 +11,8 @@ fs1 = s3fs.S3FileSystem(
       }
    )
 lineitem = ds.dataset(base, filesystem=fs1,format="parquet",partitioning="hive")
-st.write(base)
+st.write(lineitem.files)
 SQL = st.text_input('Write a SQL Query', 'select  *  from lineitem where year = 2000 limit 1')
-
 con=duckdb.connect()
 df = con.execute(SQL).df()
 st.write(df)

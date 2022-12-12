2aca5485ac7301a8b63963af254ca8f18dd794d6
Mimoune <djouallah@users.noreply.github.com>
2022-12-12 18:12:14 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
 set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
 set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  ;
 SET s3_url_style='path';
-create or replace view lineitem as select  *  from parquet_scan('s3://delta/lineitem/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1)
+create view lineitem IF NOT EXISTS as select  *  from parquet_scan('s3://delta/lineitem/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1)
 ''')
 
 SQL = st.text_input('Write a SQL Query', 'describe  lineitem')

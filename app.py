4f86c846a5e7d209245cf85b1fdb397f82302d75
Mimoune <djouallah@users.noreply.github.com>
2022-12-12 18:32:17 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ import streamlit as st
 import duckdb
 con=duckdb.connect()
 #make sure you don't include http into the endpoint
-con.execute(f'''
-install httpfs;
-LOAD httpfs;
-set s3_region = 'auto';
-set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
-set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
-set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  ;
-SET s3_url_style='path';
-create or replace view lineitem as select  *  from parquet_scan('s3://delta/lineitem/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1)
-''')
-
+@st.experimental_singleton
+def define_view():
+    con.execute(f'''
+    install httpfs;
+    LOAD httpfs;
+    set s3_region = 'auto';
+    set s3_access_key_id = "{st.secrets["aws_access_key_id_secret"]}" ;
+    set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
+    set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  ;
+    SET s3_url_style='path';
+    create or replace view lineitem as select  *  from parquet_scan('s3://delta/lineitem/*/*.parquet' , HIVE_PARTITIONING = 1,filename= 1)
+    ''')
+    return con
+con=define_view()
 SQL = st.text_input('Write a SQL Query', 'describe  lineitem')
-
-df  =con.execute(SQL).df()
+@st.experimental_memo
+def get_data(SQL):
+  return con.execute(SQL).df()
+df = get_data(SQL)  
 st.write(df)

ac4082ddb52d8a43930dde61c562fafeba49c168
Mimoune <djouallah@users.noreply.github.com>
2023-08-09 08:12:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def define_view():
     set s3_secret_access_key = '{st.secrets["aws_secret_access_key_secret"] }';
     set s3_endpoint = '{st.secrets["endpoint_url_secret"]}'  ;
     SET s3_url_style='path';
-    create or replace view scada as select * from  parquet_scan('s3://aemo/aemo/scada/data/*/*.parquet', HIVE_PARTITIONING=true) ;
+    create or replace view scada as select * from  parquet_scan('s3://aemo/scada/*/*.parquet', HIVE_PARTITIONING=true) ;
     ''')
     return con
 con=define_view()
